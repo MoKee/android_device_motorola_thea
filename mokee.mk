07e5fde075e5dd2f10adc90c7a70e26a9b0fcf82
martincz <martincz@mokeedev.com>
2016-11-21 19:47:47 +0800

    thea: mokee bringup

--- mokee.mk
+++ mokee.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/thea/full_thea.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := thea
-PRODUCT_NAME := cm_thea
+PRODUCT_NAME := mk_thea
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
